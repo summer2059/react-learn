20b33db9c5636135ca57b69bde05dc9f77d10e3c
sagar gautam <summersharma226@gmail.com>
2025-12-16 14:10:01 +0545

    using hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,60 @@
-import Test from "./test";
+// import Test from "./test";
+
+// function App() {
+
+//   const name = "React";
+//   return (
+//     <><Test/>
+//     <h1>hr { name }</h1>
+//     </>
+//   )
+// }
+
+// export default App
+
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
 
-  const name = "React";
+
+  let [counter, setCount] = useState(5)
+
+  // let counter = 5;
+
+  const addValue = () => { 
+    // counter += 1;
+    setCount(counter + 1);
+    // console.log("Counter Value: ", counter);
+  }
+
+  // const removeValue = () => { 
+  //   // counter -= 1;
+  //   setCount(prev => (prev > 0 ? prev - 1 : 0));
+  //   // console.log("Counter Value: ", counter);
+  // }
+
+  const removeValue = () => {
+    if (counter > 0) {
+      setCount(counter - 1);
+    }
+  }
   return (
-    <><Test/>
-    <h1>hr { name }</h1>
+    <>
+      <h1>Hello buddy</h1>
+      <h2>Counter Value: {counter}</h2>
+
+      <button
+      onClick={addValue}
+      >Increment</button>
+      <br />
+      <button
+      onClick={removeValue}
+      // disabled={counter === 0}
+      >Remove Value</button>
     </>
   )
 }
-
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,33 +1,45 @@
-import React, { StrictMode } from 'react'
+// import React, { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import App from './App.jsx'
+
+// // const reactElement = {
+// //   type: 'a',
+// //   props: {
+// //     href: 'https://www.example.com',
+// //     target: '_blank',
+// //     children: 'Go to Example.com'
+// //   }
+// // }
+
+// const anotherReactElement = (
+//   <a href='https://www.example.com' target='_blank'>
+//     Go to Example.com
+//   </a>
+// )
+
+// const user = "John Doe";
+// const reactElement = React.createElement(
+//   'a',
+//   { href: 'https://www.example.com', target: '_blank' },
+//   'Go to Example.com1111',
+//   user
+// );
+
+// createRoot(document.getElementById('root')).render(
+//     <App />
+
+//     // anotherReactElement
+//     // reactElement
+// )
+
+
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import './index.css'
 import App from './App.jsx'
 
-// const reactElement = {
-//   type: 'a',
-//   props: {
-//     href: 'https://www.example.com',
-//     target: '_blank',
-//     children: 'Go to Example.com'
-//   }
-// }
-
-const anotherReactElement = (
-  <a href='https://www.example.com' target='_blank'>
-    Go to Example.com
-  </a>
-)
-
-const user = "John Doe";
-const reactElement = React.createElement(
-  'a',
-  { href: 'https://www.example.com', target: '_blank' },
-  'Go to Example.com1111',
-  user
-);
-
 createRoot(document.getElementById('root')).render(
+  <StrictMode>
     <App />
-
-    // anotherReactElement
-    // reactElement
+  </StrictMode>,
 )
