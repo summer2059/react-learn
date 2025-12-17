d656f7052ead5ea151614aa5c96bdc427a189d84
sagar gautam <summersharma226@gmail.com>
2025-12-17 15:56:00 +0545

    usestte, props, and useeffec

--- src/App.jsx
+++ src/App.jsx
@@ -1,68 +1,34 @@
-// import Test from "./test";
+import { useEffect, useState } from "react";
 
-// function App() {
+export default function App() {
+  const [advice, setAdvice] = useState("");
+  const [clicks, setClicks] = useState(0);
 
-//   const name = "React";
-//   return (
-//     <><Test/>
-//     <h1>hr { name }</h1>
-//     </>
-//   )
-// }
-
-// export default App
-
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Card from './components/card'
-
-function App() {
-
-
-  let [counter, setCount] = useState(5)
-
-  // let counter = 5;
-
-  const addValue = () => { 
-    // counter += 1;
-    setCount(counter + 1);
-    // console.log("Counter Value: ", counter);
-  }
-
-  // const removeValue = () => { 
-  //   // counter -= 1;
-  //   setCount(prev => (prev > 0 ? prev - 1 : 0));
-  //   // console.log("Counter Value: ", counter);
-  // }
-
-  const removeValue = () => {
-    if (counter > 0) {
-      setCount(counter - 1);
+  async function fetchAdvice() {
+    try {
+      const response = await fetch("https://api.adviceslip.com/advice");
+      const data = await response.json();
+      setAdvice(data.slip.advice);
+      setClicks((c) => c + 1);
+    } catch (error) {
+      console.error("Error fetching advice:", error);
     }
   }
+  useEffect(function (){
+    fetchAdvice()
+  }, [])
   return (
-    <>
-      {/* <h1 className="text-4xl font-bold text-white">
-        Tailwind v3 is working 🚀
-      </h1>
-      <h1>Hello buddy</h1>
-      <h2>Counter Value: {counter}</h2>
-
-      <button
-      onClick={addValue}
-      >Increment</button>
-      <br />
-      <button
-      onClick={removeValue}
-      // disabled={counter === 0}
-      >Remove Value</button> */}
-
-      <Card username="John Doe" btnText = "Buy Now"/>
-      <Card username= "test"  btnText= "visit me"/>
-
-    </>
+    <div>
+      <h1>{advice}</h1>
+      <button onClick={fetchAdvice}>Click</button>
+      
+      <Message clicks={clicks}/>
+    </div>
   )
 }
-export default App
+
+function Message(props) {
+  return (
+    <p>you can click the button <span>{props.clicks}</span> times</p>
+  )
+}
